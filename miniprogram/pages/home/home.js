5efc79df16d20ec72a68f3992e5a14ad133c489f
jinfa.chen <jinfa.chen@1stcs.net>
2022-12-01 17:20:47 +0800

    first commit

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,140 +1,75 @@
-// pages/contact/contact.js
-const app = getApp();
-var inputVal = '';
-var msgList = [];
-var windowWidth = wx.getSystemInfoSync().windowWidth;
-var windowHeight = wx.getSystemInfoSync().windowHeight;
-var keyHeight = 0;
-
-/**
- * 初始化数据
- */
-function initData(that) {
-  inputVal = '';
-
-  msgList = [{
-      speaker: 'server',
-      contentType: 'text',
-      content: '会议室'
-    },
-    {
-      speaker: 'customer',
-      contentType: 'text',
-      content: '预定9点半的会议室'
-    },
-    {      
-      speaker: 'server',
-      contentType: 'text',
-      content: '会议预定成功，会议id:123456789，会议链接：xxxxxxx'}
-  ]
-  that.setData({
-    msgList,
-    inputVal
-  })
-}
-
-/**
- * 计算msg总高度
- */
-// function calScrollHeight(that, keyHeight) {
-//   var query = wx.createSelectorQuery();
-//   query.select('.scrollMsg').boundingClientRect(function(rect) {
-//   }).exec();
-// }
-
+// pages/home/home.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    scrollHeight: '100vh',
-    inputBottom: 0
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
-    initData(this);
-    this.setData({
-      cusHeadIcon: app.globalData.userInfo.avatarUrl,
-    });
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow() {
+    wx.getSetting({
+      success (res) {
+        console.log(res.authSetting)
+        // res.authSetting = {
+        //   "scope.userInfo": true,
+        //   "scope.userLocation": true
+        // }
+      }
+    })
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom() {
 
   },
 
   /**
-   * 获取聚焦
+   * 用户点击右上角分享
    */
-  focus: function(e) {
-    keyHeight = e.detail.height;
-    this.setData({
-      scrollHeight: (windowHeight - keyHeight) + 'px'
-    });
-    this.setData({
-      toView: 'msg-' + (msgList.length - 1),
-      inputBottom: keyHeight + 'px'
-    })
-    //计算msg高度
-    // calScrollHeight(this, keyHeight);
+  onShareAppMessage() {
 
-  },
-
-  //失去聚焦(软键盘消失)
-  blur: function(e) {
-    this.setData({
-      scrollHeight: '100vh',
-      inputBottom: 0
-    })
-    this.setData({
-      toView: 'msg-' + (msgList.length - 1)
-    })
-
-  },
-
-  /**
-   * 发送点击监听
-   */
-  sendClick: function(e) {
-    msgList.push({
-      speaker: 'customer',
-      contentType: 'text',
-      content: e.detail.value
-    })
-    inputVal = '';
-    this.setData({
-      msgList,
-      inputVal
-    });
-
-
-  },
-
-  /**
-   * 退回上一页
-   */
-  toBackClick: function() {
-    wx.navigateBack({})
   }
-
 })
